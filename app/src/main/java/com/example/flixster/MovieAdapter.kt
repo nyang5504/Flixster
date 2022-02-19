7e8c23d86d1324de41b0d7bd71132d517af34644
nyang5504 <ninayang3.14@gmail.com>
2022-02-18 21:36:06 -0500

    Flixster part 2

--- app/src/main/java/com/example/flixster/MovieAdapter.kt
+++ app/src/main/java/com/example/flixster/MovieAdapter.kt
@@ -1,6 +1,7 @@
 package com.example.flixster
 
 import android.content.Context
+import android.content.Intent
 import android.content.res.Configuration
 import android.util.Log
 import android.view.LayoutInflater
@@ -8,9 +9,14 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
+import androidx.core.app.ActivityOptionsCompat
+import androidx.core.content.ContextCompat.startActivity
+import androidx.core.view.ViewCompat
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 
+
+const val MOVIE_EXTRA = "MOVIE_EXTRA"
 private const val TAG = "MovieAdapter"
 class MovieAdapter(private val context: Context, private val movies: List<Movie>)
     : RecyclerView.Adapter<MovieAdapter.ViewHolder>() {
@@ -31,11 +37,14 @@ class MovieAdapter(private val context: Context, private val movies: List<Movie>
 
     override fun getItemCount() = movies.size
 
-    inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+    inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView), View.OnClickListener {
         private val ivPoster = itemView.findViewById<ImageView>(R.id.ivPoster)
         private val tvTitle = itemView.findViewById<TextView>(R.id.tvTitle)
         private val tvOverview = itemView.findViewById<TextView>(R.id.tvOverview)
 
+        init {
+            itemView.setOnClickListener(this)
+        }
         fun bind(movie: Movie) {
             tvTitle.text = movie.title
             tvOverview.text = movie.overview
@@ -51,6 +60,25 @@ class MovieAdapter(private val context: Context, private val movies: List<Movie>
             }
 
             //Glide.with(context).load(movie.posterImageUrl).into(ivPoster)
+        }
+
+        override fun onClick(p0: View?) {
+            // 1. Get notified of the particular movie which was clicked
+            val movie = movies[adapterPosition]
+            //Toast.makeText(context, movie.title, Toast.LENGTH_SHORT).show()
+            // 2. Use the intent system to navigate to the new activity
+            val intent = Intent(context, DetailActivity::class.java)
+            intent.putExtra(MOVIE_EXTRA, movie)
+            context.startActivity(intent)
+
+            /*
+            val tintent = Intent(context, DetailActivity::class.java)
+            // Pass data object in the bundle and populate details activity.
+            intent.putExtra(MOVIE_EXTRA, movie)
+            val options = ActivityOptionsCompat.makeSceneTransitionAnimation(this, tvTitle, ViewCompat.getTransitionName(tvTitle))
+            startActivity(tintent, options.toBundle())
+
+             */
         }
 
     }
